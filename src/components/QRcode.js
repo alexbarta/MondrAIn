d472fed7e4a107cccc4a581f23e24ba165d5ee9d
Alessandro Bartalucci <alessandrobartalucci@iMac-di-Alessandro.local>
2021-04-28 21:46:12 +0200

    using now awesome qr brunetti

--- src/components/QRcode.js
+++ src/components/QRcode.js
@@ -1,13 +1,13 @@
-import shiba from '../img/dogecoin.jpeg'
+//import shiba from '../img/dogecoin.jpeg'
 
 const { AwesomeQR } = require('awesome-qr')
 
 export const getQRBuffer = async (text) => {
   const buffer = await new AwesomeQR({
     text: text,
-    // size: 500,
-    // gifBackground: dogeBuffer,
-    backgroundImage: shiba,
+    size: 300,
+    //gifBackground: dogeBuffer,
+    //backgroundImage: shiba,
     backgroundDimming: 'rgba(255,255,255,0.2)'
   }).draw()
   return buffer
